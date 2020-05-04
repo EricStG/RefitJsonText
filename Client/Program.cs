1c288731d24b6e31ab2ca76c06e85890ed27c893
Eric St-Georges <gh@thevedge.net>
2020-05-04 19:31:04 -0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public interface ITestService
+    {
+        [Post("/api/test")]
+        Task PostAsync([Body]string[] values);
+    }
+
+    class Program
+    {
+        static async Task Main()
+        {
+            var settings = new RefitSettings(new SystemTextJsonContentSerializer());
+            var client = RestService.For<ITestService>("http://localhost:5000/", settings);
+
+            // Works: const int count = 20000;
+            const int count = 25000;
+            const string value = "a";
+            var values = Enumerable.Repeat(value, count).ToArray();
+            await client.PostAsync(values);
+        }
+    }
+}
---
+++ Server/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server
+{
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string[] values)
+        {
+            Console.WriteLine(values.Length);
+        }
+    }
+}
